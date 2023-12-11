870e626da7462f76e630d7735ffe775f821fa98d
Mike K <mkimweb@outlook.com>
2023-12-11 16:20:14 +0100

    first commit

---
+++ my-first-app/src/components/DrinkButtons.jsx
@@ -0,0 +1,12 @@
+import "./DrinkButtons.css";
+
+export const DrinkButtons = () => {
+  return (
+    <>
+      <div className="button-group">
+        <button className="button">Tea</button>
+        <button className="button">Coffee</button>
+      </div>
+    </>
+  );
+};
